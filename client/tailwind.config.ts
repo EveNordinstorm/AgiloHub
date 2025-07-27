1c69b2e5a50e3bc90d858c938b1e205a169bb136
evenordinstorm <evenordinstorm@gmail.com>
2025-07-27 11:26:06 +0100

    chore: colors and fonts tailwind classes

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,7 +7,20 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["MontserratAlternates-Regular"],
+        "montserrat-bold": ["MontserratAlternates-Bold"],
+        "montserrat-semibold": ["MontserratAlternates-SemiBold"],
+      },
+      colors: {
+        primaryBlue: "#0047AB",
+        darkBlue: "#171623",
+        primaryPurple: "#726DB3",
+        darkPurple: "#34324F",
+        yellow: "#F8E23B",
+      },
+    },
   },
   plugins: [],
 };
