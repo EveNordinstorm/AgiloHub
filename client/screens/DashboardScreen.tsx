8200a1b7f63b9e65b6dcb0acfa24f814670360b7
evenordinstorm <evenordinstorm@gmail.com>
2025-08-03 02:00:36 +0100

    feat: dashboard hero

--- client/screens/DashboardScreen.tsx
+++ client/screens/DashboardScreen.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
+import { AgiloHubIcon } from "../components/AgiloHubIcon";
 
 export default function DashboardScreen() {
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <ScrollView className="bg-darkBlue">
+      <View className="flex-row items-center py-10">
+        <View className="w-24 h-42 mx-4">
+          <AgiloHubIcon fill="#fff" />
+        </View>
+        <Text className="text-white text-3xl leading-[1.3] font-montserrat-bold">
+          Eve{"\n"}Nordinstorm's{"\n"}Agile Hub
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
