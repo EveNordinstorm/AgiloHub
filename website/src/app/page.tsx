02aeaf3d3764c3e7d7d538dd0701be024c17cdc7
evenordinstorm <evenordinstorm@gmail.com>
2025-09-17 22:57:13 +0100

    feat: init features section on home

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -62,6 +62,27 @@ export default function Home() {
             }),
           }}
         />
+
+        <div className="bg-dark-blue">
+          <section className="bg-primary-blue py-16 lg:py-26 px-8 md:px-14 lg:px-20 xl:px-26 [clip-path:polygon(0%_0%,100%_10%,100%_100%,0%_100%)]">
+            <h2 className="text-lg md:text-2xl text-white font-bold text-center mt-8 mb-5 md:mb-12">
+              {`Gamify your tasks so your agile projects become motivational for your whole team`}
+            </h2>
+            <div className="md:flex justify-between items-center">
+              <ul className="list-disc text-white font-semibold text-lg md:text-xl lg:text-2xl mx-5 md:mx-10">
+                <li>Team projects</li>
+                <li>Member connections</li>
+                <li>Includes all Agile Methodologies</li>
+                <li>Work and personal projects</li>
+                <li>Mobile and desktop access</li>
+                <li>Rewarding productivity</li>
+              </ul>
+              <div className="bg-gray-100 w-full h-60 mt-10">
+                <p className="p-5 font-bold">Placeholder for images</p>
+              </div>
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
