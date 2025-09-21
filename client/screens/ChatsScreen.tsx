2016d30945ae34cd9d369d79a7c8cb761ab84453
evenordinstorm <evenordinstorm@gmail.com>
2025-09-21 16:26:56 +0100

    feat: chat placeholder

--- client/screens/ChatsScreen.tsx
+++ client/screens/ChatsScreen.tsx
@@ -3,7 +3,16 @@ import { View, Text } from "react-native";
 export default function ChatsScreen() {
   return (
     <View>
-      <Text>Chats</Text>
+      <Text className="font-montserrat-bold text-center text-white text-3xl mb-5">
+        Chats
+      </Text>
+      <View className="mx-5">
+        <View className="bg-primaryPurple/25 p-6 rounded-2xl">
+          <Text className="font-montserrat-semibold text-white">
+            Coming Soon...
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
