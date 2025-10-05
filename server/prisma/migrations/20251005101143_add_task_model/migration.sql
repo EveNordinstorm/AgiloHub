c41222633557f1e5149f9f55429133d1f25eccb6
evenordinstorm <evenordinstorm@gmail.com>
2025-10-05 11:12:47 +0100

    feat: add tasks table

---
+++ server/prisma/migrations/20251005101143_add_task_model/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "TaskType" AS ENUM ('project', 'personal');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "points" INTEGER NOT NULL,
+    "deadline" TIMESTAMP(3),
+    "type" "TaskType" NOT NULL,
+    "complete" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "projectId" TEXT,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
