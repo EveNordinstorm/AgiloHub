0e6b96bd46cfd9e98dfd1438a2561b08e1ed100c
evenordinstorm <evenordinstorm@gmail.com>
2025-10-12 17:39:54 +0100

    feat: fetch tasks on web

--- website/src/Components/Tasks/taskCard.tsx
+++ website/src/Components/Tasks/taskCard.tsx
@@ -1,12 +1,39 @@
+import { Task } from "common/types/interfaces/task";
 import { Clock } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-export function TaskCard() {
+type TaskCardProps = {
+  task: Task;
+};
+
+export function TaskCard({ task }: TaskCardProps) {
   return (
     <div className="bg-dark-purple text-white md:w-[500px] p-5">
-      <p className="font-bold text-lg mb-2">Build nav bar component</p>
-      <p>Use Shadcn menu component, then style with Tailwind to...</p>
+      <div className="flex justify-between">
+        <p className="font-bold text-lg mb-2">{task.title}</p>
+        <div className="flex items-center px-4 py-1 rounded-full bg-primary-blue my-3">
+          <FontAwesomeIcon
+            icon={faStar}
+            className="text-lg md:text-xl text-yellow"
+          />
+          <div className="text-yellow font-bold text-lg ml-2">
+            {task.points}
+          </div>
+        </div>
+      </div>
+
+      <p>{task.description}</p>
       <div className="flex justify-end items-center gap-2">
-        <p className="text-lg font-bold">2d</p>
+        <p className="font-bold">
+          {new Date(task.deadline).toLocaleString(undefined, {
+            day: "2-digit",
+            month: "2-digit",
+            year: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </p>
         <Clock name="chevron-right" color="white" size={20} />
       </div>
     </div>
--- website/src/Components/Tasks/tasksContainer.tsx
+++ website/src/Components/Tasks/tasksContainer.tsx
@@ -1,13 +1,32 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "common/src/hooks/hooks";
+import { fetchTasks } from "common/src/redux/slices/taskSlice";
 import { TaskCard } from "./taskCard";
 
 export function TasksContainer() {
+  const dispatch = useAppDispatch();
+  const { tasks, loading, error } = useAppSelector((state) => state.task);
+
+  useEffect(() => {
+    if (!tasks.length && !loading) {
+      dispatch(fetchTasks());
+    }
+  }, [dispatch, loading, tasks]);
+
+  if (loading) return <p className="text-white">Loading tasks...</p>;
+  if (error) return <p className="text-red-500">{error}</p>;
+
   return (
     <div className="px-5 md:px-10">
       <div className="bg-black p-6 md:p-8">
         <h2 className="text-white font-bold text-xl md:text-2xl mb-4">
           My Tasks
         </h2>
-        <TaskCard />
+        <div className="grid gap-6 md:grid-cols-2">
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task} />
+          ))}
+        </div>
       </div>
     </div>
   );
