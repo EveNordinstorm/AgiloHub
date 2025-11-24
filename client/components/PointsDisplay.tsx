0fa0fa72f42dbaf3f181ad495af08273c0126994
evenordinstorm <evenordinstorm@gmail.com>
2025-11-24 21:41:20 +0000

    feat: display user's current total points

--- client/components/PointsDisplay.tsx
+++ client/components/PointsDisplay.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { useAppDispatch, useAppSelector } from "common/src/hooks/hooks";
+import { fetchPointsTotal } from "common/src/redux/slices/pointsSlice";
 
 export default function PointsDisplay() {
+  const dispatch = useAppDispatch();
+
+  const total = useAppSelector((state) => state.points.total);
+  const user = useAppSelector((state) => state.auth.user);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchPointsTotal());
+    }
+  }, [user]);
+
   return (
     <View className="ml-5 flex-row gap-2 bg-primaryBlue px-2 rounded-full py-1 items-center">
       <FontAwesome name="star" size={22} color="#F8E23B" />
-      <Text className="text-yellow font-montserrat-bold text-lg">1000</Text>
+      <Text className="text-yellow font-montserrat-bold text-lg">{total}</Text>
     </View>
   );
 }
