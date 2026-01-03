ea99103263c01a573401a3138331978e96062de3
evenordinstorm <evenordinstorm@gmail.com>
2026-01-03 23:52:54 +0000

    feat: timeline interfaces

---
+++ common/src/types/enums/stageIcon.ts
@@ -0,0 +1,5 @@
+export enum StageIcon {
+  ARROW_DOWN = "ARROW_DOWN",
+  REFRESH_CW = "REFRESH_CW",
+  FLAG = "FLAG",
+}
--- common/src/types/interfaces/projectStage.ts
+++ common/src/types/interfaces/projectStage.ts
@@ -1,7 +1,21 @@
+import { StageIcon } from "../enums/stageIcon";
+
 export type ProjectStage = {
+  id: string;
   stageNumber: number;
   description: string;
   pointsEarned: number;
   totalPoints: number;
   date: string;
+  icon: StageIcon;
+  completed: boolean;
+  completedAt?: string;
+  completedById?: string;
+};
+
+export type ProjectStageInput = {
+  description: string;
+  totalPoints: number;
+  date: string;
+  icon: StageIcon;
 };
